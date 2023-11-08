f362f0e327f3732967236d764dafda9bce6a5bfb
Bonie Mwangi <mboniface107@gmail.com>
2023-11-08 09:39:07 +0300

    update entrypoint file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import { MongoClient } from 'mongodb';
-
+import {MongoClient} from 'mongodb';
 import sendNotification from "./notification.js";
 import {env} from "./constants.js"; // Import the config function
-
-
-const { MONGO_DB_URL, MONGO_DB_NAME } = env   // Access environment variables using process.env
-
+const {MONGO_DB_URL, MONGO_DB_NAME} = env   // Access environment variables using process.env
 async function getDB() {
     try {
 
-        const client = new MongoClient(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect().then(()=>{
+        const client = new MongoClient(MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+        await client.connect().then(() => {
             console.log('DB Connected ...')
         })
         return client.db(MONGO_DB_NAME);
@@ -27,8 +23,8 @@ export const start = async () => {
         const invoices = await db.collection('invoices').find({}).toArray();
         const tasks = invoices.map((invoice) =>
             new Promise((resolve, reject) => {
-                const  dueDate  =  Date.parse(invoice.due_date);
-                const today =   Date.parse("2023-11-18");  //Date.now();
+                const dueDate = Date.parse(invoice.due_date);
+                const today = Date.now();
                 if (today >= dueDate) {
                     sendNotification(invoice)
                         .then(resolve)
@@ -41,6 +37,6 @@ export const start = async () => {
         await Promise.all(tasks);
     };
     console.log('Starting notification cycle...');
-    runNotificationCycle()
-  //  setInterval(runNotificationCycle, 86400000);
+    //runNotificationCycle()
+    setInterval(runNotificationCycle, 86400000);
 };
